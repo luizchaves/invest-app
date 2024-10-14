622674a6f08828d33b91f4346789b855d823ca6b
Luiz Chaves <lucachaves@gmail.com>
2024-10-13 23:06:53 -0300

    feat: add express-oas-generator

--- src/index.js
+++ src/index.js
@@ -2,12 +2,15 @@ import 'express-async-errors';
 import express from 'express';
 import cors from 'cors';
 import morgan from 'morgan';
-import swaggerUi from 'swagger-ui-express';
-import fs from 'node:fs';
+// import swaggerUi from 'swagger-ui-express';
+// import fs from 'node:fs';
+import expressOasGenerator from 'express-oas-generator';
 
 import router from './routes.js';
 
 const server = express();
+
+expressOasGenerator.handleResponses(app, {});
 
 server.use(morgan('tiny'));
 
@@ -25,9 +28,11 @@ server.use(
 
 server.use('/api', router);
 
-const swaggerJson = JSON.parse(fs.readFileSync('openapi.json'));
+// const swaggerJson = JSON.parse(fs.readFileSync('openapi.json'));
 
-server.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerJson));
+// server.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerJson));
+
+expressOasGenerator.handleRequests();
 
 server.listen(3000, () => {
   console.log('Server is running on port 3000');
